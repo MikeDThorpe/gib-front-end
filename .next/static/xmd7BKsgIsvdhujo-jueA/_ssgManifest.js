5aec43298825f002be86bc7b7b625566e895e9b4
Michael Thorpe <47759631+MikeDThorpe@users.noreply.github.com>
2022-04-20 16:09:41 +0100

    Delete .next directory

--- .next/static/xmd7BKsgIsvdhujo-jueA/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Frecipes\u002F[category]","\u002Frecipes\u002Ftags\u002F[tag]","\u002Frecipes\u002F[category]\u002F[recipe]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
